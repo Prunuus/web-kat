9fa12f3846644118f42b533ff700830ebbb6cb8a
Hurdamert <lichengtx@gmail.com>
2024-08-22 15:01:57 -0500

    Donation page and button

---
+++ app/Donations/page.js
@@ -0,0 +1,8 @@
+export default function page(){
+    return(
+        <section>
+            <h1>Have donation stuff here</h1>
+            <h2>Waluigi</h2>
+        </section>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,3 +1,14 @@
+'use client'
+import "./globals.css";
+import { useRouter } from 'next/navigation';
+
+
 export default function Page() {
-  return <h1>This is a cat</h1>
+  const router = useRouter();
+  return(
+    <div>
+      <h1>Welcome to Ags Reach Gift Drive!</h1>
+      <button type="button" onClick={() => router.push('./Donations')}>Donate!</button>
+    </div>
+  )
 }
