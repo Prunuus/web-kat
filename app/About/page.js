f9855939e65086353becdda6ba1ccb8a29d146db
Hurdamert <lichengtx@gmail.com>
2024-08-28 17:06:45 -0500

    some main page stuff

---
+++ app/About/page.js
@@ -0,0 +1,7 @@
+export default function page(){
+    return(
+        <section>
+            <p>About page here</p>
+        </section>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -7,8 +7,15 @@ export default function Page() {
   const router = useRouter();
   return(
     <div>
-      <h1>Welcome to Ags Reach Gift Drive!</h1>
-      <button type="button" onClick={() => router.push('./Donations')}>Donate!</button>
+      <img className="logo"src="https://reach4home.org/wp-content/uploads/2021/06/The-Reach-Project-Logo-Regular.jpg" alt="reach image"></img>
+      <h1 className ="welcome">Welcome to Ags Reach Gift Drive!</h1>
+
+      <div className="navi">
+        <a onClick={() => router.push('..')}>Home</a>
+        <a onClick={() => router.push('./About')}>About</a>
+        <a onClick={() => router.push('./Donations')}>Donate!</a>
+      </div>
+
     </div>
   )
 }
