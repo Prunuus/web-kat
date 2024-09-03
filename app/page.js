2357b04db527ed3db1c966b2d6cb8f2ffa6b3f04
Hurdamert <lichengtx@gmail.com>
2024-09-03 15:02:45 -0500

    finalize nav deco

--- app/page.js
+++ app/page.js
@@ -6,15 +6,16 @@ import { useRouter } from 'next/navigation';
 export default function Page() {
   const router = useRouter();
   return(
-    <div>
+    <div className ="navi">
       <img className="logo"src="https://reach4home.org/wp-content/uploads/2021/06/The-Reach-Project-Logo-Regular.jpg" alt="reach image"></img>
-      <h1 className ="welcome">Welcome to Ags Reach Gift Drive!</h1>
 
-      <div className="navi">
-        <a onClick={() => router.push('..')}>Home</a>
-        <a onClick={() => router.push('./About')}>About</a>
-        <a onClick={() => router.push('./Donations')}>Donate!</a>
-      </div>
+        <a href="https://web-kat.vercel.app" onClick={() => router.push('..')} className="nav-links1">Home</a>
+        <a href="https://web-kat.vercel.app/About" onClick={() => router.push('./About')} className="nav-links2">About</a>
+        <a href = "https://web-kat.vercel.app/Donations"onClick={() => router.push('./Donations')}className="nav-links3">Donate!</a>
+
+        <div className="welcome-area">
+          <h1 className ="welcome">Welcome to Ags Reach Gift Drive!</h1>
+        </div>
 
     </div>
   )
